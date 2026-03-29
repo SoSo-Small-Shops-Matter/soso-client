2c12e14ae571adc9f5db05814d41c4a76d6eced9
heroine <juyen3409@gmail.com>
2026-03-29 23:39:28 +0900

    feat: user profile query에 staletime, token enabled 추가

--- apps/soso-web/src/shared/api/user/queries.ts
+++ apps/soso-web/src/shared/api/user/queries.ts
@@ -1,9 +1,10 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useAuthStore } from '@/shared/store/useAuthStore';
 import { useDialog } from '@/shared/context/DialogContext';
 import { useRouter } from 'next/navigation';
 import { userApi } from './api';
 import type { DeleteUserRequest, PatchUserRequestType } from './types';
+import { HOUR } from '@repo/utils';
 
 export const userKeys = {
   all: ['user'] as const,
@@ -11,11 +12,15 @@ export const userKeys = {
   duplicateNickname: (nickname: string) => [...userKeys.all, 'duplicateNickname', nickname] as const,
 };
 
-export const useGetUserProfileQuery = () =>
-  useQuery({
+export const useGetUserProfileQuery = () => {
+  const { token } = useAuthStore();
+  return useQuery({
     queryKey: userKeys.profile(),
     queryFn: userApi.getUserProfile,
+    enabled: !!token,
+    staleTime: 1 * HOUR
   });
+};
 
 export const useGetDuplicateNicknameQuery = (nickname: string) =>
   useQuery({
@@ -24,11 +29,16 @@ export const useGetDuplicateNicknameQuery = (nickname: string) =>
     enabled: !!nickname,
   });
 
-export const usePatchUserProfileMutation = () =>
-  useMutation({
+export const usePatchUserProfileMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
     mutationKey: ['patchUserProfile'],
     mutationFn: (data: PatchUserRequestType) => userApi.patchUserProfile(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: userKeys.profile() });
+    }
   });
+};
 
 export const useDeleteUserMutation = () => {
   const router = useRouter();
