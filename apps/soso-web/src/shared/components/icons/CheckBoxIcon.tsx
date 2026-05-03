d3597662e68e37c3d0aefc80557f37b3a5e2dc08
오현재 <hyunjae1995@gmail.com>
2026-05-03 13:32:10 +0900

    fix: svg 속성 camelcase로

--- apps/soso-web/src/shared/components/icons/CheckBoxIcon.tsx
+++ apps/soso-web/src/shared/components/icons/CheckBoxIcon.tsx
@@ -14,7 +14,7 @@ export default function CheckIcon({ width, height, fill, checked }: CheckBoxIcon
       fill="none"
     >
       <rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" fill="#FF7F50" />
-      <rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" stroke="#FF7F50" stroke-width="1.5" />
+      <rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" stroke="#FF7F50" strokeWidth="1.5" />
       <path
         d="M15.1133 6.38714C15.4517 6.7256 15.4517 7.27434 15.1133 7.61279L9.11328 13.6128C8.77483 13.9512 8.22609 13.9512 7.88763 13.6128L4.88763 10.6128C4.54918 10.2743 4.54918 9.7256 4.88763 9.38714C5.22608 9.04869 5.77483 9.04869 6.11328 9.38714L8.50046 11.7743L13.8876 6.38714C14.2261 6.04869 14.7748 6.04869 15.1133 6.38714Z"
         fill={fill || 'white'}
@@ -29,7 +29,7 @@ export default function CheckIcon({ width, height, fill, checked }: CheckBoxIcon
       fill="none"
     >
       <rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" fill="#E8EBED" />
-      <rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" stroke="#E8EBED" stroke-width="1.5" />
+      <rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" stroke="#E8EBED" strokeWidth="1.5" />
       <path
         d="M15.1133 6.38714C15.4517 6.7256 15.4517 7.27434 15.1133 7.61279L9.11328 13.6128C8.77483 13.9512 8.22609 13.9512 7.88763 13.6128L4.88763 10.6128C4.54918 10.2743 4.54918 9.7256 4.88763 9.38714C5.22608 9.04869 5.77483 9.04869 6.11328 9.38714L8.50046 11.7743L13.8876 6.38714C14.2261 6.04869 14.7748 6.04869 15.1133 6.38714Z"
         fill={fill || 'white'}
